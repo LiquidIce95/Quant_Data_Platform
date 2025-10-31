edcf74437ff4ac165f99ed910902ed525bb9ba9f
david <david.sanchez@omnicommediagroup.com>
2025-10-31 15:14:15 +0000

    d

--- services_streaming_lane/ib_connector/source/src/main/scala/Boilerplate/play.scala
+++ services_streaming_lane/ib_connector/source/src/main/scala/Boilerplate/play.scala
@@ -8,8 +8,7 @@ import sttp.client4.quick
 object play {
 	def main(args: Array[String]): Unit = {
 		val response: Response[String] = quickRequest
-		  	.header("Host", "localhost")
-			.get(uri"https://client-portal.client-portal-api.svc:5000/v1/api/iserver/auth/status")
+			.get(uri"https://localhost:5000/v1/api/iserver/auth/status")
 			.send()
 
 		println(response.code)
